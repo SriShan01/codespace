c497de3e315940234f15f7f5a7dbc7636da77e40
SriShan01 <126793611+SriShan01@users.noreply.github.com>
2023-03-08 12:42:58 +0000

    Wed, Mar 8, 2023, 12:42 PM +00:00

--- mario.c
+++ mario.c
@@ -6,7 +6,32 @@ int main(void)
     int i =0;
     for (i=0; i<3; i++)
     {
-        printf("#")
+        printf("#");
     }
     printf("\n");
+
+    int j =0;
+    for (j=0; j<3; j++)
+    {
+        printf("#");
+    }
+    printf("\n");
+
+    int k =0;
+    for (k=0; k<3; k++)
+    {
+        printf("#");
+    }
+    printf("\n");
+
+    int x= 0;
+    for (x=0; x<3; x++)
+    {
+        int y = 0;
+        for (y=0; y<3; y++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }

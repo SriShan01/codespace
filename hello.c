b813d99942b083d62daeb064d64d1d9f5af0d199
SriShan01 <126793611+SriShan01@users.noreply.github.com>
2023-03-08 08:52:01 +0000

    Wed, Mar 8, 2023, 8:52 AM +00:00

--- hello.c
+++ hello.c
@@ -7,5 +7,19 @@ int main(void)
     string last = get_string("Please type your last name? ");
     printf("hello %s %s\n", first, last);
 
-    if
+    int x = get_int("Type x value: ");
+    int y = get_int("Type y value: ");
+
+    if (x<y)
+    {
+        printf("x is less than y");
+    }
+    else if (x>y)
+    {
+        printf("x is greater than y");
+    }
+    else
+    {
+        printf("x is equal to y");
+    }
 }
